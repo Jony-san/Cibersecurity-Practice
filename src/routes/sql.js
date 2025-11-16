1e956b1e6457c48912f433baac24f2129cf2141d
Adryo-joni <jsantos@adryo.com.mx>
2025-11-15 18:10:11 -0600

    Ejemplo de sql

---
+++ src/routes/sql.js
@@ -0,0 +1,30 @@
+//Importar framework
+import express from 'express';
+//Importar conexion a la base de datos
+import conection from '../db/database.js';
+
+const router = express.Router();
+
+//ruta de ejemplo de una inyeccion sql
+//POST http://localhost:3003/sql/inyection
+router.post('/inyection', async (req, res) => {
+    try{
+        console.log("iniciando ejemplo de inyeccion sql");
+        const userdata = req.body;
+        console.log("data recibida", req.body);
+    
+        console.log("realizando consulta del usuario");
+        const usuario = await conection.query(`select * from usuario where nombre = '${userdata.username}'`);
+        console.log("data encontrada", usuario);
+
+    //if(usuario)
+
+    res.json({ usuario });
+    }catch(error){
+        console.error('Error en la consulta:', error);
+        res.status(500).json({ error: 'Error en la consulta' });
+    }
+
+});
+
+export default router;
